78a15df6fbf744a54a997d08ce9e0a87c32a7413
santimartinez1717 <124884731+santimartinez1717@users.noreply.github.com>
2025-05-15 17:31:37 +0200

    db changed

--- myFirstApiRest/myFirstApiRest/settings.py
+++ myFirstApiRest/myFirstApiRest/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "myFirstApiRest.wsgi.application"
 
 load_dotenv()
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
